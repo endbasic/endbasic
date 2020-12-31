b2de0aa6a0bdb7c3ea71e3f50b67f5f3f1d3a8e3
Julio Merino <julio@meroh.net>
2020-12-31 12:33:36 -0800

    Disable new clippy::await_holding_refcell_ref
    There is no easy way to fix this warning right now, as far as I can tell,
    so allow it given that the default in Clippy is to allow it too.
    
    As part of this, make the health GitHub Actions workflow use the most
    recent version of Clippy so that we can catch these errors earlier, and
    so that the allow() setting I just added works.

--- core/tests/integration_test.rs
+++ core/tests/integration_test.rs
@@ -19,6 +19,7 @@
 // Should probably add a separate testutils crate, but I'm hesitant for now.
 
 // Keep these in sync with other top-level files.
+#![allow(clippy::await_holding_refcell_ref)]
 #![warn(anonymous_parameters, bad_style, missing_docs)]
 #![warn(unused, unused_extern_crates, unused_import_braces, unused_qualifications)]
 #![warn(unsafe_code)]
