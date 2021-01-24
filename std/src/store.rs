86480cc5271df734d13bf5721f62988f4fbaebb2
Julio Merino <julio@meroh.net>
2021-01-23 20:43:33 -0800

    Move the disk-based store to std
    This follows the previous change to move the crossterm-based console
    from the cli to std so that using the standard library is easier and
    does the same thing for the file-backed store.
    
    Once again, I'm unsure if putting this directly in the std crate is
    the best idea, but in this case the move seems harmless because this
    feature doesn't have any dependencies that don't build in a WASM
    context.

--- std/src/store.rs
+++ std/src/store.rs
@@ -22,7 +22,8 @@ use endbasic_core::eval::{CallableMetadata, CallableMetadataBuilder};
 use endbasic_core::exec::{self, Command, Machine};
 use std::cell::RefCell;
 use std::collections::{BTreeMap, HashMap};
-use std::io;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, Read, Write};
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::str;
@@ -93,6 +94,85 @@ impl Store for InMemoryStore {
     fn put(&mut self, name: &str, content: &str) -> io::Result<()> {
         self.programs.insert(name.to_owned(), content.to_owned());
         Ok(())
+    }
+}
+
+/// An implementation of `Store` backed by an on-disk directory.
+pub struct FileStore {
+    /// Path to the directory containing all entries backed by this `Store`.  The directory may
+    /// contain files that are not EndBASIC programs, and that's OK, but those files will not be
+    /// accessible through this interface.
+    dir: PathBuf,
+}
+
+impl FileStore {
+    /// Creates a new store backed by the `dir` directory.
+    pub fn new<P: Into<PathBuf>>(dir: P) -> Self {
+        Self { dir: dir.into() }
+    }
+}
+
+impl Store for FileStore {
+    fn delete(&mut self, name: &str) -> io::Result<()> {
+        let path = self.dir.join(name);
+        fs::remove_file(path)
+    }
+
+    fn enumerate(&self) -> io::Result<BTreeMap<String, Metadata>> {
+        let mut entries = BTreeMap::default();
+        match fs::read_dir(&self.dir) {
+            Ok(dirents) => {
+                for de in dirents {
+                    let de = de?;
+
+                    let file_type = de.file_type()?;
+                    if !file_type.is_file() && !file_type.is_symlink() {
+                        // Silently ignore entries we cannot handle.
+                        continue;
+                    }
+
+                    // This follows symlinks for cross-platform simplicity, but it is ugly.  I don't
+                    // expect symlinks in the programs directory anyway.  If we want to handle this
+                    // better, we'll have to add a way to report file types.
+                    let metadata = fs::metadata(de.path())?;
+                    let offset = match time::UtcOffset::try_current_local_offset() {
+                        Ok(offset) => offset,
+                        Err(_) => time::UtcOffset::UTC,
+                    };
+                    let date = time::OffsetDateTime::from(metadata.modified()?).to_offset(offset);
+                    let length = metadata.len();
+
+                    entries.insert(
+                        de.file_name().to_string_lossy().to_string(),
+                        Metadata { date, length },
+                    );
+                }
+            }
+            Err(e) => {
+                if e.kind() != io::ErrorKind::NotFound {
+                    return Err(e);
+                }
+            }
+        }
+        Ok(entries)
+    }
+
+    fn get(&self, name: &str) -> io::Result<String> {
+        let path = self.dir.join(name);
+        let input = File::open(&path)?;
+        let mut content = String::new();
+        io::BufReader::new(input).read_to_string(&mut content)?;
+        Ok(content)
+    }
+
+    fn put(&mut self, name: &str, content: &str) -> io::Result<()> {
+        let path = self.dir.join(name);
+        let dir = path.parent().expect("Must be a filename with a directory");
+        fs::create_dir_all(&dir)?;
+
+        let output = OpenOptions::new().create(true).write(true).truncate(true).open(path)?;
+        let mut writer = io::BufWriter::new(output);
+        writer.write_all(content.as_bytes())
     }
 }
 
@@ -522,6 +602,138 @@ pub fn add_all(
 mod tests {
     use super::*;
     use crate::testutils::*;
+    use std::fs;
+    use std::io::{BufRead, Write};
+    use std::path::Path;
+
+    /// Reads `path` and checks that its contents match `exp_lines`.
+    fn check_file(path: &Path, exp_lines: &[&str]) {
+        let file = File::open(path).unwrap();
+        let reader = io::BufReader::new(file);
+        let mut lines = vec![];
+        for line in reader.lines() {
+            lines.push(line.unwrap());
+        }
+        assert_eq!(exp_lines, lines.as_slice());
+    }
+
+    /// Creates `path` with the contents in `lines` and with a deterministic modification time.
+    fn write_file(path: &Path, lines: &[&str]) {
+        let mut file = fs::OpenOptions::new()
+            .create(true)
+            .truncate(false) // Should not be creating the same file more than once.
+            .write(true)
+            .open(path)
+            .unwrap();
+        for line in lines {
+            file.write_fmt(format_args!("{}\n", line)).unwrap();
+        }
+        drop(file);
+
+        filetime::set_file_mtime(path, filetime::FileTime::from_unix_time(1_588_757_875, 0))
+            .unwrap();
+    }
+
+    #[test]
+    fn test_filestore_delete_ok() {
+        let dir = tempfile::tempdir().unwrap();
+        write_file(&dir.path().join("a.bas"), &[]);
+        write_file(&dir.path().join("a.bat"), &[]);
+
+        let mut store = FileStore::new(&dir.path());
+        store.delete("a.bas").unwrap();
+        assert!(!dir.path().join("a.bas").exists());
+        assert!(dir.path().join("a.bat").exists());
+    }
+
+    #[test]
+    fn test_filestore_delete_missing_file() {
+        let dir = tempfile::tempdir().unwrap();
+        let mut store = FileStore::new(&dir.path());
+        assert_eq!(io::ErrorKind::NotFound, store.delete("a.bas").unwrap_err().kind());
+    }
+
+    #[test]
+    fn test_filestore_enumerate_nothing() {
+        let dir = tempfile::tempdir().unwrap();
+
+        let store = FileStore::new(&dir.path());
+        assert!(store.enumerate().unwrap().is_empty());
+    }
+
+    #[test]
+    fn test_filestore_enumerate_some_files() {
+        let dir = tempfile::tempdir().unwrap();
+        write_file(&dir.path().join("empty.bas"), &[]);
+        write_file(&dir.path().join("some file.bas"), &["this is not empty"]);
+
+        let store = FileStore::new(&dir.path());
+        let entries = store.enumerate().unwrap();
+        assert_eq!(2, entries.len());
+        let date = time::OffsetDateTime::from_unix_timestamp(1_588_757_875);
+        assert_eq!(&Metadata { date, length: 0 }, entries.get("empty.bas").unwrap());
+        assert_eq!(&Metadata { date, length: 18 }, entries.get("some file.bas").unwrap());
+    }
+
+    #[test]
+    fn test_filestore_enumerate_treats_missing_dir_as_empty() {
+        let dir = tempfile::tempdir().unwrap();
+        let store = FileStore::new(dir.path().join("does-not-exist"));
+        assert!(store.enumerate().unwrap().is_empty());
+    }
+
+    #[test]
+    fn test_filestore_enumerate_ignores_non_files() {
+        let dir = tempfile::tempdir().unwrap();
+        fs::create_dir(dir.path().join("will-be-ignored")).unwrap();
+        let store = FileStore::new(&dir.path());
+        assert!(store.enumerate().unwrap().is_empty());
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    #[test]
+    fn test_filestore_enumerate_follows_symlinks() {
+        use std::os::unix::fs as unix_fs;
+
+        let dir = tempfile::tempdir().unwrap();
+        write_file(&dir.path().join("some file.bas"), &["this is not empty"]);
+        unix_fs::symlink(&Path::new("some file.bas"), &dir.path().join("a link.bas")).unwrap();
+
+        let store = FileStore::new(&dir.path());
+        let entries = store.enumerate().unwrap();
+        assert_eq!(2, entries.len());
+        let metadata =
+            Metadata { date: time::OffsetDateTime::from_unix_timestamp(1_588_757_875), length: 18 };
+        assert_eq!(&metadata, entries.get("some file.bas").unwrap());
+        assert_eq!(&metadata, entries.get("a link.bas").unwrap());
+    }
+
+    #[test]
+    fn test_filestore_enumerate_fails_on_non_directory() {
+        let dir = tempfile::tempdir().unwrap();
+        let file = dir.path().join("not-a-dir");
+        write_file(&file, &[]);
+        let store = FileStore::new(&file);
+        assert_eq!(io::ErrorKind::Other, store.enumerate().unwrap_err().kind());
+    }
+
+    #[test]
+    fn test_filestore_get() {
+        let dir = tempfile::tempdir().unwrap();
+        write_file(&dir.path().join("some file.bas"), &["one line", "two lines"]);
+
+        let store = FileStore::new(&dir.path());
+        assert_eq!("one line\ntwo lines\n", store.get("some file.bas").unwrap());
+    }
+
+    #[test]
+    fn test_filestore_put() {
+        let dir = tempfile::tempdir().unwrap();
+
+        let mut store = FileStore::new(&dir.path());
+        store.put("some file.bas", "a b c\nd e\n").unwrap();
+        check_file(&dir.path().join("some file.bas"), &["a b c", "d e"]);
+    }
 
     #[test]
     fn test_del_ok() {
