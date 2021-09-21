1768d8b066a83dfc9d1355f2fd424d821ba0fcf5
Julio Merino <julio@meroh.net>
2021-09-21 06:22:09 -0700

    Enable the sdl feature in official builds
    This modifies the GitHub workflows so that the SDL2 console is checked
    (via clippy for now, as there are no tests yet) and enables its build
    for official builds.
    
    This is tricky due to the need to install the SDL2 libraries on the
    build machines on the platforms we support.  Also, on macOS, this
    makes the resulting builds very difficult to use due to the lack of
    code signing.  We'll need to figure this out and/or provide builds
    without graphics too.

---
+++ .github/workflows/setup-sdl.ps1
@@ -0,0 +1,36 @@
+# EndBASIC
+# Copyright 2021 Julio Merino
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+Invoke-WebRequest `
+    -Uri https://www.libsdl.org/release/SDL2-devel-2.0.16-VC.zip `
+    -OutFile SDL2-devel-2.0.16-VC.zip
+Invoke-WebRequest `
+    -Uri https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-VC.zip `
+    -OutFile SDL2_ttf-devel-2.0.15-VC.zip
+
+unzip SDL2-devel-2.0.16-VC.zip
+unzip SDL2_ttf-devel-2.0.15-VC.zip
+
+New-Item -Type Directory libs
+Copy-Item .\SDL2-2.0.16\lib\x64\*.lib,.\SDL2_ttf-2.0.15\lib\x64\*.lib libs
+New-Item -Type Directory dlls
+Copy-Item .\SDL2-2.0.16\lib\x64\*.dll,.\SDL2_ttf-2.0.15\lib\x64\*.dll dlls
+Copy-Item .\SDL2-2.0.16\lib\x64\*.txt,.\SDL2_ttf-2.0.15\lib\x64\*.txt dlls
+
+Remove-Item -Recurse -Force .\SDL2-2.0.16,.\SDL2_ttf-2.0.15,SDL2*.zip
+
+New-Item -Type Directory target,target\debug,target\release
+Copy-Item dlls\* target\debug
+Copy-Item dlls\* target\release
---
+++ cli/build.rs
@@ -0,0 +1,27 @@
+// EndBASIC
+// Copyright 2021 Julio Merino
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+use std::env;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        // Point linker to the libraries prepared by setup-sdl.ps1.
+        let current_dir = env::current_dir().unwrap();
+        let libs = current_dir.parent().unwrap().join("libs");
+        if libs.exists() {
+            println!("cargo:rustc-link-search={}", libs.display());
+        }
+    }
+}
