b184b5a687e03a36beabd6c1b842ab58bad9b93b
Julio Merino <julio@meroh.net>
2021-09-30 06:12:09 -0700

    Add defaults for the graphical console spec
    When configuring the graphical console from the CLI, make all parameters
    optional.
    
    This means we have to include a default font as well, which is a bit
    tricky because SDL2 needs to load it from disk.  We cope with this by
    writing out the default font to a temporary file and deleting it as
    soon as the console is up and running.

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -127,7 +127,16 @@ pub async fn run_repl_loop(
 #[cfg(feature = "sdl")]
 pub fn setup_graphics_console(spec: &str) -> io::Result<Rc<RefCell<dyn Console>>> {
     let spec = sdl::parse_graphics_spec(spec)?;
-    Ok(Rc::from(RefCell::from(SdlConsole::new(spec.0, spec.1, spec.2, spec.3)?)))
+    let console = match spec.2 {
+        None => {
+            let default_font = sdl::TempFont::default_font()?;
+            SdlConsole::new(spec.0, spec.1, &default_font.path(), spec.3)?
+            // The console has been created at this point, so it should be safe to drop
+            // default_font and clean up the on-disk file backing it up.
+        }
+        Some(font_path) => SdlConsole::new(spec.0, spec.1, font_path, spec.3)?,
+    };
+    Ok(Rc::from(RefCell::from(console)))
 }
 
 /// Errors out during the creation of the graphical console when SDL support is not compiled in.
