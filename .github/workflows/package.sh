f56e3e995233ba9e2b8a1872ce810616914d8ba5
Julio Merino <julio@meroh.net>
2021-10-22 14:32:11 -0700

    Redo release-time packaging tests
    The previous "cargo package" tests were incomplete and annoying to deal
    with because they only tested the "core" crate and only validated the
    other crates after they had already been pushed out.  So... they weren't
    really testing that the crates could be packaged.
    
    Redo this by simulating "cargo package" runs (without the actual
    "publish" step) against a local index.  This is contrived and I'm not
    sure it's a great test, but it's better than being completely blind to
    problems.  That said, the new test is so heavy (15+ minutes, primarily
    due to the cargo dependencies) that I'm relegating it to run at release
    time only.

--- .github/workflows/package.sh
+++ .github/workflows/package.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # EndBASIC
-# Copyright 2020 Julio Merino
+# Copyright 2021 Julio Merino
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License.  You may obtain a copy
@@ -14,29 +14,52 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+# Verify that "cargo publish" has good chances of succeeding for all of our crates.
+#
+# This is very hard to do and is not well-documented, so to avoid pulling in very
+# heavy dependencies, this tries to simulate the above by "just" packaging the
+# crates (a la "cargo package") and adding them to a local-only index.
+#
+# WARNING: These checks are extremely expensive and only run at release time.
+# WARNING: Make sure to follow the instructions in RELEASE.md for these to apply.
+
 set -eux
 
-# Test the installation workflow first because this should succeed regardless
-# of the state of crates.io.
-cargo install --path cli
-ret=0; echo "EXIT 123" | "${HOME}/.cargo/bin/endbasic" || ret="$?"
-if [ "${ret}" -ne 123 ]; then
-    echo "Installed endbasic binary doesn't seem to work" 1>&2
-    exit 1
-fi
+cargo install cargo-index cargo-local-registry
 
-( cd core && cargo publish --dry-run )
+# Build once to generate a Cargo.lock file.
+cargo build --all-features
+cargo clean
 
-# If we aren't yet ready to release a new version, all crates that depend
-# on others in the workspace cannot be built in publish mode.  Skip them
-# until we can test them.
-if grep 'Changes in' NEWS.md | head -n 1 | fgrep 'X.Y.Z'; then
-    echo "Skipping endbasic publish test in development version"
-else
-    ( cd std && cargo publish --dry-run )
-    ( cd repl && cargo publish --dry-run )
-    ( cd terminal && cargo publish --dry-run )
-    ( cd sdl && cargo publish --dry-run )
-    ( cd rpi && cargo publish --dry-run )
-    ( cd cli && cargo publish --dry-run )
-fi
+# Initialize a local registry based on the Cargo.lock file.
+registry="$(pwd)/registry"
+git config --global user.email "nobody@example.com"
+git config --global user.name "Nobody"
+cargo local-registry --sync . "${registry}"
+(
+    cd "${registry}/index"
+    git init
+    git add *
+    git commit -a -m 'Initialize local registry'
+)
+
+# Configure Cargo overrides to use the local registry.
+cat >.cargo/config <<EOF
+[source.crates-io]
+registry = 'https://github.com/rust-lang/crates.io-index'
+replace-with = 'local-registry'
+
+[source.local-registry]
+local-registry = '${registry}'
+EOF
+
+# Package one crate at a time and add it to the local registry so that subsequent crates
+# can pick them up.
+for dir in core std repl terminal sdl rpi cli; do
+    cd "${dir}"
+    cargo index add --index "${registry}/index" --index-url https://example.com
+    cd -
+    if [ "${dir}" != cli ]; then
+        cp target/package/endbasic-"${dir}"-*.crate "${registry}"
+    fi
+done
