96ab0b87815e4c7353c36bb3f02cf14ed1eaae64
Julio Merino <julio@meroh.net>
2021-10-25 06:03:38 -0700

    Break apart the web lib.rs into various files
    This separates all of the console implementation out of the web's lib.rs
    file and moves the xterm.js-related code to a new xterm.js and the input
    handling code to a new input.js.  Keeps the main file more readable and
    will make it easier to swap out the console implementation for a new one.

---
+++ web/src/input.rs
@@ -0,0 +1,139 @@
+// EndBASIC
+// Copyright 2021 Julio Merino
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+//! Keyboard input tools for the web UI.
+
+use async_channel::{self, Receiver, Sender, TryRecvError};
+use endbasic_std::console::Key;
+use std::io;
+use wasm_bindgen::prelude::*;
+use xterm_js_rs::OnKeyEvent;
+
+/// Converts an xterm.js key event into our own `Key` representation.
+fn on_key_event_into_key(event: OnKeyEvent) -> Key {
+    let dom_event = event.dom_event();
+    match dom_event.key_code() as u8 {
+        8 => Key::Backspace,
+        9 => Key::Tab,
+        10 => Key::NewLine,
+        13 => Key::CarriageReturn,
+        27 => Key::Escape,
+        35 => Key::End,
+        36 => Key::Home,
+        37 => Key::ArrowLeft,
+        38 => Key::ArrowUp,
+        39 => Key::ArrowRight,
+        40 => Key::ArrowDown,
+        b'A' if dom_event.ctrl_key() => Key::Home,
+        b'B' if dom_event.ctrl_key() => Key::ArrowLeft,
+        b'C' if dom_event.ctrl_key() => Key::Interrupt,
+        b'D' if dom_event.ctrl_key() => Key::Eof,
+        b'E' if dom_event.ctrl_key() => Key::End,
+        b'F' if dom_event.ctrl_key() => Key::ArrowRight,
+        b'J' if dom_event.ctrl_key() => Key::NewLine,
+        b'M' if dom_event.ctrl_key() => Key::NewLine,
+        b'N' if dom_event.ctrl_key() => Key::ArrowDown,
+        b'P' if dom_event.ctrl_key() => Key::ArrowUp,
+        _ => {
+            let printable = !dom_event.alt_key() && !dom_event.ctrl_key() && !dom_event.meta_key();
+            let chars = event.key().chars().collect::<Vec<char>>();
+            if printable && chars.len() == 1 {
+                Key::Char(chars[0])
+            } else {
+                Key::Unknown(format!("<keycode={}>", dom_event.key_code()))
+            }
+        }
+    }
+}
+
+/// Interface to implement an on-screen keyboard to provide keys that may not be available on
+/// mobile keyboards.
+#[wasm_bindgen]
+pub struct OnScreenKeyboard {
+    on_key_tx: Sender<Key>,
+}
+
+#[wasm_bindgen]
+impl OnScreenKeyboard {
+    /// Generates a fake Escape key press.
+    pub fn press_escape(&self) {
+        self.on_key_tx.try_send(Key::Escape).expect("Send to unbounded channel must succeed")
+    }
+
+    /// Generates a fake arrow up key press.
+    pub fn press_arrow_up(&self) {
+        self.on_key_tx.try_send(Key::ArrowUp).expect("Send to unbounded channel must succeed")
+    }
+
+    /// Generates a fake arrow down key press.
+    pub fn press_arrow_down(&self) {
+        self.on_key_tx.try_send(Key::ArrowDown).expect("Send to unbounded channel must succeed")
+    }
+
+    /// Generates a fake arrow left key press.
+    pub fn press_arrow_left(&self) {
+        self.on_key_tx.try_send(Key::ArrowLeft).expect("Send to unbounded channel must succeed")
+    }
+
+    /// Generates a fake arrow up key press.
+    pub fn press_arrow_right(&self) {
+        self.on_key_tx.try_send(Key::ArrowRight).expect("Send to unbounded channel must succeed")
+    }
+}
+
+/// Interface to interact with the browser's input, be it via a real keyboard or our custom
+/// on-screen keyboard.
+pub struct WebInput {
+    on_key_rx: Receiver<Key>,
+    on_key_tx: Sender<Key>,
+}
+
+impl Default for WebInput {
+    fn default() -> Self {
+        let (on_key_tx, on_key_rx) = async_channel::unbounded();
+        Self { on_key_rx, on_key_tx }
+    }
+}
+
+impl WebInput {
+    /// Generates a closure that xterm.js can use to handle key events.
+    pub(crate) fn terminal_on_key(&self) -> Closure<dyn FnMut(OnKeyEvent)> {
+        let on_key_tx = self.on_key_tx.clone();
+        Closure::wrap(Box::new(move |e| {
+            on_key_tx
+                .try_send(on_key_event_into_key(e))
+                .expect("Send to unbounded channel must succeed")
+        }) as Box<dyn FnMut(OnKeyEvent)>)
+    }
+
+    /// Generates a new `OnScreenKeyboard` that can inject key events.
+    pub(crate) fn on_screen_keyboard(&self) -> OnScreenKeyboard {
+        OnScreenKeyboard { on_key_tx: self.on_key_tx.clone() }
+    }
+
+    /// Gets the next key event, if one is available.
+    pub(crate) async fn try_recv(&mut self) -> io::Result<Option<Key>> {
+        match self.on_key_rx.try_recv() {
+            Ok(k) => Ok(Some(k)),
+            Err(TryRecvError::Empty) => Ok(None),
+            Err(TryRecvError::Closed) => panic!("Channel unexpectedly closed"),
+        }
+    }
+
+    /// Gets the next key event, waiting until one is available.
+    pub(crate) async fn recv(&mut self) -> io::Result<Key> {
+        Ok(self.on_key_rx.recv().await.unwrap())
+    }
+}
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -22,28 +22,28 @@
 #![warn(unused, unused_extern_crates, unused_import_braces, unused_qualifications)]
 #![warn(unsafe_code)]
 
-use async_channel::TryRecvError;
-#[cfg(test)]
-use wasm_bindgen_test::wasm_bindgen_test_configure;
-#[cfg(test)]
-wasm_bindgen_test_configure!(run_in_browser);
-
-mod store;
-
-use async_trait::async_trait;
 use endbasic_core::syms::{self, CommandResult};
-use endbasic_std::console::{CharsXY, ClearType, Console, Key};
+use endbasic_std::console::Console;
 use std::cell::RefCell;
-use std::cmp::Ordering;
-use std::convert::TryFrom;
 use std::future::Future;
-use std::io;
 use std::pin::Pin;
 use std::rc::Rc;
 use std::time::Duration;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use xterm_js_rs::{OnKeyEvent, Terminal};
+#[cfg(test)]
+use wasm_bindgen_test::wasm_bindgen_test_configure;
+use xterm_js_rs::Terminal;
+
+#[cfg(test)]
+wasm_bindgen_test_configure!(run_in_browser);
+
+mod input;
+use input::{OnScreenKeyboard, WebInput};
+mod store;
+use store::WebDriveFactory;
+mod xterm;
+use xterm::XtermJsConsole;
 
 /// Sleeps for the given period of time.
 fn js_sleep(d: Duration) -> Pin<Box<dyn Future<Output = CommandResult>>> {
@@ -80,219 +80,11 @@ fn js_sleep(d: Duration) -> Pin<Box<dyn Future<Output = CommandResult>>> {
     })
 }
 
-/// Converts an xterm.js key event into our own `Key` representation.
-fn on_key_event_into_key(event: OnKeyEvent) -> Key {
-    let dom_event = event.dom_event();
-    match dom_event.key_code() as u8 {
-        8 => Key::Backspace,
-        9 => Key::Tab,
-        10 => Key::NewLine,
-        13 => Key::CarriageReturn,
-        27 => Key::Escape,
-        35 => Key::End,
-        36 => Key::Home,
-        37 => Key::ArrowLeft,
-        38 => Key::ArrowUp,
-        39 => Key::ArrowRight,
-        40 => Key::ArrowDown,
-        b'A' if dom_event.ctrl_key() => Key::Home,
-        b'B' if dom_event.ctrl_key() => Key::ArrowLeft,
-        b'C' if dom_event.ctrl_key() => Key::Interrupt,
-        b'D' if dom_event.ctrl_key() => Key::Eof,
-        b'E' if dom_event.ctrl_key() => Key::End,
-        b'F' if dom_event.ctrl_key() => Key::ArrowRight,
-        b'J' if dom_event.ctrl_key() => Key::NewLine,
-        b'M' if dom_event.ctrl_key() => Key::NewLine,
-        b'N' if dom_event.ctrl_key() => Key::ArrowDown,
-        b'P' if dom_event.ctrl_key() => Key::ArrowUp,
-        _ => {
-            let printable = !dom_event.alt_key() && !dom_event.ctrl_key() && !dom_event.meta_key();
-            let chars = event.key().chars().collect::<Vec<char>>();
-            if printable && chars.len() == 1 {
-                Key::Char(chars[0])
-            } else {
-                Key::Unknown(format!("<keycode={}>", dom_event.key_code()))
-            }
-        }
-    }
-}
-
-/// Implementation of a console that talks directly to an xterm.js terminal.
-struct XtermJsConsole {
-    terminal: Terminal,
-    on_key_rx: async_channel::Receiver<Key>,
-    alt_active: bool,
-}
-
-impl XtermJsConsole {
-    fn new(terminal: Terminal, on_key_rx: async_channel::Receiver<Key>) -> Self {
-        Self { terminal, on_key_rx, alt_active: false }
-    }
-}
-
-#[async_trait(?Send)]
-impl Console for XtermJsConsole {
-    fn clear(&mut self, how: ClearType) -> io::Result<()> {
-        match how {
-            ClearType::All => {
-                self.terminal.write("\u{001b}[2J");
-                self.terminal.write("\u{001b}[0;0H");
-            }
-            ClearType::CurrentLine => {
-                self.terminal.write("\u{001b}[2K");
-            }
-            ClearType::PreviousChar => {
-                self.terminal.write("\x08 \x08");
-            }
-            ClearType::UntilNewLine => {
-                self.terminal.write("\u{001b}[K");
-            }
-        }
-        Ok(())
-    }
-
-    fn color(&mut self, fg: Option<u8>, bg: Option<u8>) -> io::Result<()> {
-        match fg {
-            None => self.terminal.write("\u{001b}[39m"),
-            Some(color) => self.terminal.write(&format!("\u{001b}[38;5;{}m", color)),
-        };
-        match bg {
-            None => self.terminal.write("\u{001b}[49m"),
-            Some(color) => self.terminal.write(&format!("\u{001b}[48;5;{}m", color)),
-        };
-        self.terminal.write("\u{001b}[0K");
-        Ok(())
-    }
-
-    fn enter_alt(&mut self) -> io::Result<()> {
-        if !self.alt_active {
-            self.terminal.write("\u{001b}[?1049h");
-            self.alt_active = true;
-        }
-        Ok(())
-    }
-
-    fn hide_cursor(&mut self) -> io::Result<()> {
-        self.terminal.write("\u{001b}[?25l");
-        Ok(())
-    }
-
-    fn is_interactive(&self) -> bool {
-        true
-    }
-
-    fn leave_alt(&mut self) -> io::Result<()> {
-        if self.alt_active {
-            self.terminal.write("\u{001b}[?1049l");
-            self.alt_active = false;
-        }
-        Ok(())
-    }
-
-    fn locate(&mut self, pos: CharsXY) -> io::Result<()> {
-        self.terminal.write(&format!("\u{001b}[{};{}H", pos.y + 1, pos.x + 1));
-        Ok(())
-    }
-
-    fn move_within_line(&mut self, off: i16) -> io::Result<()> {
-        match off.cmp(&0) {
-            Ordering::Less => self.terminal.write(&format!("\u{001b}[{}D", -off)),
-            Ordering::Equal => (),
-            Ordering::Greater => self.terminal.write(&format!("\u{001b}[{}C", off)),
-        }
-        Ok(())
-    }
-
-    fn print(&mut self, text: &str) -> io::Result<()> {
-        debug_assert!(!endbasic_std::console::has_control_chars_str(text));
-
-        self.terminal.write(text);
-        self.terminal.write("\r\n");
-        Ok(())
-    }
-
-    async fn poll_key(&mut self) -> io::Result<Option<Key>> {
-        match self.on_key_rx.try_recv() {
-            Ok(k) => Ok(Some(k)),
-            Err(TryRecvError::Empty) => Ok(None),
-            Err(TryRecvError::Closed) => panic!("Channel unexpectedly closed"),
-        }
-    }
-
-    async fn read_key(&mut self) -> io::Result<Key> {
-        Ok(self.on_key_rx.recv().await.unwrap())
-    }
-
-    fn show_cursor(&mut self) -> io::Result<()> {
-        self.terminal.write("\u{001b}[?25h");
-        Ok(())
-    }
-
-    fn size(&self) -> io::Result<CharsXY> {
-        Ok(CharsXY::new(
-            u16::try_from(self.terminal.get_cols()).unwrap(),
-            u16::try_from(self.terminal.get_rows()).unwrap(),
-        ))
-    }
-
-    fn write(&mut self, bytes: &[u8]) -> io::Result<()> {
-        debug_assert!(!endbasic_std::console::has_control_chars_u8(bytes));
-
-        // TODO(jmmv): Should not have to convert to UTF-8 here because it might not be and the
-        // terminal should not care (?).
-        self.terminal.write(&String::from_utf8_lossy(bytes));
-        Ok(())
-    }
-
-    fn sync_now(&mut self) -> io::Result<()> {
-        Ok(())
-    }
-
-    fn set_sync(&mut self, _enabled: bool) -> io::Result<()> {
-        Ok(())
-    }
-}
-
-/// Interface to implement an on-screen keyboard to provide keys that may not be available on
-/// mobile keyboards.
-#[wasm_bindgen]
-pub struct OnScreenKeyboard {
-    on_key_tx: async_channel::Sender<Key>,
-}
-
-#[wasm_bindgen]
-impl OnScreenKeyboard {
-    /// Generates a fake Escape key press.
-    pub fn press_escape(&self) {
-        self.on_key_tx.try_send(Key::Escape).expect("Send to unbounded channel must succeed")
-    }
-
-    /// Generates a fake arrow up key press.
-    pub fn press_arrow_up(&self) {
-        self.on_key_tx.try_send(Key::ArrowUp).expect("Send to unbounded channel must succeed")
-    }
-
-    /// Generates a fake arrow down key press.
-    pub fn press_arrow_down(&self) {
-        self.on_key_tx.try_send(Key::ArrowDown).expect("Send to unbounded channel must succeed")
-    }
-
-    /// Generates a fake arrow left key press.
-    pub fn press_arrow_left(&self) {
-        self.on_key_tx.try_send(Key::ArrowLeft).expect("Send to unbounded channel must succeed")
-    }
-
-    /// Generates a fake arrow up key press.
-    pub fn press_arrow_right(&self) {
-        self.on_key_tx.try_send(Key::ArrowRight).expect("Send to unbounded channel must succeed")
-    }
-}
-
 /// Sets up the common storage drives.
 fn setup_storage(storage: &mut endbasic_std::storage::Storage) {
     storage.register_scheme("demos", Box::from(endbasic_repl::demos::DemoDriveFactory::default()));
     storage.mount("demos", "demos://").expect("Demos drive shouldn't fail to mount");
-    storage.register_scheme("local", Box::from(store::WebDriveFactory::default()));
+    storage.register_scheme("local", Box::from(WebDriveFactory::default()));
     storage.mount("local", "local://").expect("Web drive shouldn't fail to mount");
     storage.cd("local:").expect("Local drive was just registered");
 }
@@ -300,8 +92,7 @@ fn setup_storage(storage: &mut endbasic_std::storage::Storage) {
 /// Connects the EndBASIC interpreter to a web page.
 #[wasm_bindgen]
 pub struct WebTerminal {
-    on_key_rx: async_channel::Receiver<Key>,
-    on_key_tx: async_channel::Sender<Key>,
+    input: WebInput,
 }
 
 #[wasm_bindgen]
@@ -310,28 +101,20 @@ impl WebTerminal {
     #[wasm_bindgen(constructor)]
     #[allow(clippy::new_without_default)] // Cannot implement Default in wasm-bindgen.
     pub fn new() -> Self {
-        let (on_key_tx, on_key_rx) = async_channel::unbounded();
-        Self { on_key_rx, on_key_tx }
+        Self { input: WebInput::default() }
     }
 
     /// Generates a new `OnScreenKeyboard` that can inject keypresses into this terminal.
     pub fn on_screen_keyboard(&self) -> OnScreenKeyboard {
-        OnScreenKeyboard { on_key_tx: self.on_key_tx.clone() }
+        self.input.on_screen_keyboard()
     }
 
     /// Starts the EndBASIC interpreter loop on the specified `terminal`.
     pub async fn run_repl_loop(self, terminal: Terminal) {
-        let (on_key_tx, on_key_rx) = (self.on_key_tx, self.on_key_rx);
-        let on_key_callback = {
-            Closure::wrap(Box::new(move |e| {
-                on_key_tx
-                    .try_send(on_key_event_into_key(e))
-                    .expect("Send to unbounded channel must succeed")
-            }) as Box<dyn FnMut(OnKeyEvent)>)
-        };
+        let on_key_callback = self.input.terminal_on_key();
         terminal.on_key(on_key_callback.as_ref().unchecked_ref());
 
-        let console = Rc::from(RefCell::from(XtermJsConsole::new(terminal, on_key_rx)));
+        let console = Rc::from(RefCell::from(XtermJsConsole::new(terminal, self.input)));
         let mut builder = endbasic_std::MachineBuilder::default()
             .with_console(console.clone())
             .with_sleep_fn(Box::from(js_sleep))
