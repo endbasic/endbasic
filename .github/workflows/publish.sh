b037e86c137b1a25f1b8fdd48346da8f89268da8
Julio Merino <julio@meroh.net>
2026-04-24 18:00:00 -0700

    Replace wasm-pack plugin with wasm-bindgen builds
    Build the web package with explicit cargo and wasm-bindgen commands
    before invoking webpack.  Remove the unmaintained webpack plugin,
    install cargo-binstall in deploy workflows, and use it to install
    wasm-bindgen-cli while keeping wasm-pack for the existing test command.

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -28,6 +28,9 @@ nvm install --lts
 set -ux
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+rustup target add wasm32-unknown-unknown
+wasm_bindgen_version="$(grep '^wasm-bindgen = ' web/Cargo.toml | cut -d '"' -f 2)"
+cargo binstall --no-confirm "wasm-bindgen-cli@${wasm_bindgen_version}"
 
 export NVM_DIR="${HOME}/.nvm"
 [ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
