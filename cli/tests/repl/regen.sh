17735c354b8c83d90ad7a9f6a82baa23325ee0ee
Julio Merino <julio@meroh.net>
2026-04-24 22:52:54 -0700

    Refactor tests for examples
    In order to test the examples, we need to load each example into the
    interpreter, run it, and perform some post-validation steps.
    
    Right now, this is done by passing a script to the interpreter in
    interactive mode so that the script can use program-level commands
    such as LOAD and RUN.  Unfortunately, with the core rewrite I'm
    working on, the execution model of EndBASIC changes and these commands
    *replace* the running program image.  If we have pre-compiled the
    example wrapping script, as soon as we do a LOAD of the example itself,
    the wrapper code is lost.
    
    To future-proof these tests, change them so that all wrapping code is
    fed to the interpreter as if it were REPL input.  This way, the REPL
    interprets each line individually and can see any commands that follow
    LOAD and RUN.

--- cli/tests/repl/regen.sh
+++ cli/tests/repl/regen.sh
@@ -1,3 +1,4 @@
+#! /bin/sh
 # EndBASIC
 # Copyright 2021 Julio Merino
 #
@@ -19,10 +20,14 @@ tmpdir="$(mktemp -d)"
 trap "rm -f \"${tmpdir}\"/*; rmdir \"${tmpdir}\"" EXIT
 
 run() {
+    local gpio_pins="${1}"; shift
     local local_drive="${1}"; shift
 
-    LINES=24 COLUMNS=80 cargo run -- --local-drive="${local_drive}" \
-        --interactive "${@}"
+    LINES=24 COLUMNS=80 cargo run -- \
+        --gpio-pins="${gpio_pins}" \
+        --interactive \
+        --local-drive="${local_drive}" \
+        "${@}"
 }
 
 date_re="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-2][0-9]:[0-5][0-9]"
@@ -36,32 +41,28 @@ for outfile in "${@}"; do
 
     rm -f "${tmpdir}"/*
 
-    local_drive=
+    gpio_pins=noop
+    local_drive="file://${tmpdir}"
     case "${outfile}" in
         *dir.out)
             local_drive="memory://"
             ;;
 
+        *gpio.out)
+            gpio_pins=mock
+            ;;
+
         *load-save.out)
             cp "$(dirname "${basfile}")/hello.bas" "${tmpdir}"
-            local_drive="file://${tmpdir}"
-            ;;
-
-        *storage.out)
-            local_drive="file://${tmpdir}"
-            ;;
-
-        *)
-            local_drive="file://${tmpdir}"
             ;;
     esac
 
     if [ -f "${basfile}" -a -f "${infile}" ]; then
-        run "${local_drive}" "${basfile}" <"${infile}" >"${outfile}.new"
+        run "${gpio_pins}" "${local_drive}" "${basfile}" <"${infile}" >"${outfile}.new"
     elif [ -f "${basfile}" ]; then
-        run "${local_drive}" "${basfile}" >"${outfile}.new"
+        run "${gpio_pins}" "${local_drive}" "${basfile}" >"${outfile}.new"
     elif [ -f "${infile}" ]; then
-        run "${local_drive}" <"${infile}" >"${outfile}.new"
+        run "${gpio_pins}" "${local_drive}" <"${infile}" >"${outfile}.new"
     else
         echo "No input for ${outfile}?" 1>&2
         continue
