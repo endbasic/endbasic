e596f75e27cbf0bcd8037a7d1a543201faf904d7
Julio Merino <julio@meroh.net>
2026-06-21 06:56:32 -0700

    Move SLEEP and related code to a new datetime module
    This is to make room for additional time-related commands that I want
    to implement for better frame loop control.

--- std/src/exec.rs
+++ std/src/exec.rs
@@ -18,15 +18,10 @@
 
 use async_trait::async_trait;
 use endbasic_core::{
-    ArgSepSyntax, CallError, CallResult, Callable, CallableMetadata, CallableMetadataBuilder,
-    ExprType, RequiredValueSyntax, Scope, SingularArgSyntax,
+    CallResult, Callable, CallableMetadata, CallableMetadataBuilder, ExprType, Scope,
 };
-use futures_lite::future::{BoxedLocal, FutureExt};
-use std::borrow::Cow;
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::thread;
-use std::time::Duration;
 
 use crate::{MachineAction, MachineBuilder};
 
@@ -115,82 +110,9 @@ impl Callable for ErrmsgFunction {
     }
 }
 
-/// Type of the sleep function used by the `SLEEP` command to actually suspend execution.
-pub type SleepFn = Box<dyn Fn(Duration) -> BoxedLocal<Result<(), String>>>;
-
-/// An implementation of a `SleepFn` that stops the current thread.
-fn system_sleep(d: Duration) -> BoxedLocal<Result<(), String>> {
-    async move {
-        thread::sleep(d);
-        Ok(())
-    }
-    .boxed_local()
-}
-
-/// The `SLEEP` command.
-pub struct SleepCommand {
-    metadata: Rc<CallableMetadata>,
-    sleep_fn: SleepFn,
-}
-
-impl SleepCommand {
-    /// Creates a new instance of the command.
-    pub fn new(sleep_fn: SleepFn) -> Rc<Self> {
-        Rc::from(Self {
-            metadata: CallableMetadataBuilder::new("SLEEP")
-                .with_async(true)
-                .with_syntax(&[(
-                    &[SingularArgSyntax::RequiredValue(
-                        RequiredValueSyntax {
-                            name: Cow::Borrowed("seconds"),
-                            vtype: ExprType::Double,
-                        },
-                        ArgSepSyntax::End,
-                    )],
-                    None,
-                )])
-                .with_category(CATEGORY)
-                .with_description(
-                    "Suspends program execution.
-Pauses program execution for the given number of seconds, which can be specified either as an \
-integer or as a floating point number for finer precision.",
-                )
-                .build(),
-            sleep_fn,
-        })
-    }
-}
-
-#[async_trait(?Send)]
-impl Callable for SleepCommand {
-    fn metadata(&self) -> Rc<CallableMetadata> {
-        self.metadata.clone()
-    }
-
-    async fn async_exec(&self, scope: Scope<'_>) -> CallResult<()> {
-        debug_assert_eq!(1, scope.nargs());
-        let n = scope.get_double(0);
-
-        if n < 0.0 {
-            return Err(CallError::Syntax(
-                scope.get_pos(0),
-                "Sleep time must be positive".to_owned(),
-            ));
-        }
-
-        (self.sleep_fn)(Duration::from_secs_f64(n))
-            .await
-            .map_err(|e| CallError::Syntax(scope.get_pos(0), e))
-    }
-}
-
 /// Instantiates all REPL commands for the scripting machine and adds them to the `machine`.
-///
-/// `sleep_fn` is an async function that implements a pause given a `Duration`.  If not provided,
-/// uses the `std::thread::sleep` function.
-pub fn add_scripting(machine: &mut MachineBuilder, sleep_fn: Option<SleepFn>) {
+pub fn add_scripting(machine: &mut MachineBuilder) {
     machine.add_callable(ErrmsgFunction::new());
-    machine.add_callable(SleepCommand::new(sleep_fn.unwrap_or_else(|| Box::from(system_sleep))));
 }
 
 /// Instantiates all REPL commands for the interactive machine and adds them to the `machine`.
@@ -203,10 +125,11 @@ mod tests {
     use super::*;
     use crate::testutils::*;
     use crate::{Error, MachineBuilder, Signal, Yielder};
-    use futures_lite::future::block_on;
+    use futures_lite::FutureExt;
+    use futures_lite::future::{BoxedLocal, block_on};
     use std::cell::RefCell;
     use std::rc::Rc;
-    use std::time::Instant;
+    use std::time::Duration;
 
     #[test]
     fn test_clear_ok() {
@@ -259,53 +182,6 @@ mod tests {
     fn test_errmsg_errors() {
         check_expr_compilation_error("1:10: ERRMSG expected no arguments", r#"ERRMSG()"#);
         check_expr_compilation_error("1:10: ERRMSG expected no arguments", r#"ERRMSG(3)"#);
-    }
-
-    #[test]
-    fn test_sleep_ok_int() {
-        let sleep_fake = |d: Duration| -> BoxedLocal<Result<(), String>> {
-            async move { Err(format!("Got {} ms", d.as_millis())) }.boxed_local()
-        };
-
-        let mut machine = MachineBuilder::default().with_sleep_fn(Box::from(sleep_fake)).build();
-        machine.compile(&mut "SLEEP 123".as_bytes()).unwrap();
-        assert_eq!("1:7: Got 123000 ms", format!("{}", block_on(machine.exec()).unwrap_err()));
-    }
-
-    #[test]
-    fn test_sleep_ok_float() {
-        let sleep_fake = |d: Duration| -> BoxedLocal<Result<(), String>> {
-            async move {
-                let ms = d.as_millis();
-                if ms > 123095 && ms < 123105 {
-                    Err("Good".to_owned())
-                } else {
-                    Err(format!("Bad {}", ms))
-                }
-            }
-            .boxed_local()
-        };
-
-        let mut machine = MachineBuilder::default().with_sleep_fn(Box::from(sleep_fake)).build();
-        machine.compile(&mut "SLEEP 123.1".as_bytes()).unwrap();
-        assert_eq!("1:7: Good", format!("{}", block_on(machine.exec()).unwrap_err()));
-    }
-
-    #[test]
-    fn test_sleep_real() {
-        let before = Instant::now();
-        Tester::default().run("SLEEP 0.010").check();
-        assert!(before.elapsed() >= Duration::from_millis(10));
-    }
-
-    #[test]
-    fn test_sleep_errors() {
-        check_stmt_compilation_err("1:1: SLEEP expected seconds#", "SLEEP");
-        check_stmt_compilation_err("1:1: SLEEP expected seconds#", "SLEEP 2, 3");
-        check_stmt_compilation_err("1:1: SLEEP expected seconds#", "SLEEP 2; 3");
-        check_stmt_compilation_err("1:7: STRING is not a number", "SLEEP \"foo\"");
-        check_stmt_err("1:7: Sleep time must be positive", "SLEEP -1");
-        check_stmt_err("1:7: Sleep time must be positive", "SLEEP -0.001");
     }
 
     #[test]
