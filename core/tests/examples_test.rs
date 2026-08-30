26bc0720f693897d7f9733503059dafe061c821f
Julio Merino <julio@meroh.net>
2026-08-30 06:20:02 -0700

    Migrate process tests to assert_cmd
    I just discovered this crate and it does the type of CLI testing I've
    always thought is necessary in a better way than what I implemented here.

--- core/tests/examples_test.rs
+++ core/tests/examples_test.rs
@@ -16,110 +16,37 @@
 
 //! Integration tests that use golden input and output files.
 
+use assert_cmd::Command;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
-use std::process;
-
-/// Computes the path to the cargo profile directory for this test.
-fn profile_dir() -> PathBuf {
-    let self_exe = env::current_exe().expect("Cannot get self's executable path");
-    let deps_dir = self_exe.parent().expect("Cannot get self's directory");
-    deps_dir.parent().expect("Failed to get parent directory").to_owned()
-}
-
-/// Computes the path to the built binary `name`.
-fn bin_path<P: AsRef<Path>>(name: P) -> PathBuf {
-    profile_dir().join(name).with_extension(env::consts::EXE_EXTENSION)
-}
 
 /// Computes the path to the source file `name`.
 fn src_path(name: &str) -> PathBuf {
     let dir =
         Path::new(env!("CARGO_MANIFEST_DIR")).parent().expect("Failed to get parent directory");
 
-    // Sanity-check that we landed in the right location.
     assert!(dir.join("Cargo.lock").exists());
-
     dir.join(name)
 }
 
-/// Describes the behavior for one of the three streams (stdin, stdout, stderr) connected to a
-/// program.
-enum Behavior {
-    /// Ensure the stream is silent.
-    Null,
-
-    /// If stdin, feed the given path as the program's input.  If stdout/stderr, expect the contents
-    /// of the stream to match this file.
-    File(PathBuf),
-}
-
 /// Reads the contents of a golden data file.
-fn read_golden(path: &Path) -> String {
-    let mut f = File::open(path).expect("Failed to open golden data file");
+fn read_golden(name: &str) -> String {
+    let path = src_path(name);
+    let mut f = File::open(&path).expect("Failed to open golden data file");
     let mut golden = vec![];
     f.read_to_end(&mut golden).expect("Failed to read golden data file");
     let raw = String::from_utf8(golden).expect("Golden data file is not valid UTF-8");
     if cfg!(target_os = "windows") { raw.replace("\r\n", "\n") } else { raw }
 }
 
-/// Runs `bin` with arguments `args` and checks its behavior against expectations.
-///
-/// `exp_code` is the expected error code from the program.  `stdin_behavior` indicates what to feed
-/// to the program's stdin.  `stdout_behavior` and `stderr_behavior` indicate what to expect from
-/// the program's textual output.
-fn check<P: AsRef<Path>>(
-    bin: P,
-    args: &[&str],
-    exp_code: i32,
-    stdin_behavior: Behavior,
-    stdout_behavior: Behavior,
-    stderr_behavior: Behavior,
-) {
-    let golden_stdin = match stdin_behavior {
-        Behavior::Null => process::Stdio::null(),
-        Behavior::File(path) => File::open(path).unwrap().into(),
-    };
-
-    let exp_stdout = match stdout_behavior {
-        Behavior::Null => "".to_owned(),
-        Behavior::File(path) => read_golden(&path),
-    };
-
-    let exp_stderr = match stderr_behavior {
-        Behavior::Null => "".to_owned(),
-        Behavior::File(path) => read_golden(&path),
-    };
-
-    let result = process::Command::new(bin.as_ref())
-        .args(args)
-        .stdin(golden_stdin)
-        .output()
-        .expect("Failed to execute subprocess");
-    let code = result.status.code().expect("Subprocess didn't exit cleanly");
-    let stdout = String::from_utf8(result.stdout).expect("Stdout not is not valid UTF-8");
-    let stderr = String::from_utf8(result.stderr).expect("Stderr not is not valid UTF-8");
-
-    if exp_code != code || exp_stdout != stdout || exp_stderr != stderr {
-        eprintln!("Exit code: {}", code);
-        eprintln!("stdout:\n{}", stdout);
-        eprintln!("stderr:\n{}", stderr);
-        assert_eq!(exp_code, code);
-        assert_eq!(exp_stdout, stdout);
-        assert_eq!(exp_stderr, stderr);
-    }
-}
-
 #[test]
 fn test_config() {
-    check(
-        bin_path("examples/core-config"),
-        &[],
-        0,
-        Behavior::Null,
-        Behavior::File(src_path("core/tests/config.out")),
-        Behavior::Null,
-    );
+    Command::cargo_bin("examples/core-config")
+        .unwrap()
+        .assert()
+        .success()
+        .stdout(read_golden("core/tests/config.out"))
+        .stderr("");
 }
